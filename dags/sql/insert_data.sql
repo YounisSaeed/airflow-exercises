bb4d449004bbfef87d89b8c46ee139357aa8b11c
YounisSaeed <youniszidan94@gmail.com>
2025-02-14 20:21:43 +0200

    update insert_data file

--- dags/sql/insert_data.sql
+++ dags/sql/insert_data.sql
@@ -1,2 +1,8 @@
+-- For test case (failure) to add new task on trello boards
 INSERT INTO egyptian_league_clubs (namess,name_club, year_of_construction, degree_of_level_league, num_of_trophy) 
 VALUES ('Al Ahly', 1907, 'Premier League', 42);
+
+--- For test case (insert) successfully 
+/* INSERT INTO egyptian_league_clubs (name_club, year_of_construction, degree_of_level_league, num_of_trophy) 
+VALUES ('Al Ahly', 1907, 'Premier League', 42);
+ */
